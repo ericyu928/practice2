ac744282d30a31a85179a9120951bbd978fa09b7
ericyu928 <84361126+ericyu928@users.noreply.github.com>
2022-03-04 17:25:58 +0800

    if else>case

--- src/store/stateStore.js
+++ src/store/stateStore.js
@@ -9,66 +9,68 @@ const contactState = {
 }
 
 const stateStoreReducer = (state = contactState, action) => {
-    if (action.type === "addContactTypeList") {
-        return {
-            contacttypelist: [...state.contacttypelist, action.newClass],
-            contactData: state.contactData
-        }
-    }
-    else if (action.type === "editContactTypeList") {
-        for (let i = 0; i < state.contacttypelist.length; i++) {
-            if (action.newClass.ClassId === state.contacttypelist[i].ClassId) {
-                const newItems = [...state.contacttypelist];
-                newItems[i].Name = action.newClass.Name;
-                return {
-                    contacttypelist: newItems,
-                    contactData: state.contactData
-                };
+    switch (action.type) {
+        case "addContactTypeList":
+            return {
+                contacttypelist: [...state.contacttypelist, action.newClass],
+                contactData: state.contactData
+            };
+        case "editContactTypeList":
+            for (let i = 0; i < state.contacttypelist.length; i++) {
+                if (action.newClass.ClassId === state.contacttypelist[i].ClassId) {
+                    const newItems = [...state.contacttypelist];
+                    newItems[i].Name = action.newClass.Name;
+                    return {
+                        contacttypelist: newItems,
+                        contactData: state.contactData
+                    };
+                }
             }
-        }
+            break;
+        case "deleteContactTypeList":
+            for (let i = 0; i < state.contacttypelist.length; i++) {
+                if (action.classId === state.contacttypelist[i].ClassId) {
+                    const newItems = [...state.contacttypelist];
+                    newItems.splice(i, 1);
+                    return {
+                        contacttypelist: newItems,
+                        contactData: state.contactData
+    
+                    };
+                }
+            }
+            break;
+        case "addContactList":
+            return {
+                contacttypelist: state.contacttypelist,
+                contactData: [...state.contactData, action.contact]
+            };
+        case "editContactList":
+            for (let i = 0; i < state.contactData.length; i++) {
+                if (action.contact.ContactId === state.contactData[i].ContactId) {
+                    const newItems = [...state.contactData];
+                    newItems[i] = action.contact;
+                    return {
+                        contactData: newItems,
+                        contacttypelist: state.contacttypelist,
+                    };
+                }
+            }
+            break;
+        case "delContactList":
+            for (let i = 0; i < state.contactData.length; i++) {
+                if (action.contactId === state.contactData[i].ContactId) {
+                    const newItems = [...state.contactData];
+                    newItems.splice(i, 1);
+                    return { contactData: newItems, contacttypelist: state.contacttypelist };
+                }
+            }
+            break;
+        default:
+            return state
     }
-    else if (action.type === "deleteContactTypeList") {
-        for (let i = 0; i < state.contacttypelist.length; i++) {
-            if (action.classId === state.contacttypelist[i].ClassId) {
-                const newItems = [...state.contacttypelist];
-                newItems.splice(i, 1);
-                return {
-                    contacttypelist: newItems,
-                    contactData: state.contactData
 
-                };
-            }
-        }
-    }
-    else if (action.type === "addContactList") {
-        return {
-            contacttypelist: state.contacttypelist,
-            contactData: [...state.contactData, action.contact]
-        }
-    }
-    else if (action.type === "editContactList") {
-        for (let i = 0; i < state.contactData.length; i++) {
-            if (action.contact.ContactId === state.contactData[i].ContactId) {
-                const newItems = [...state.contactData];
-                newItems[i] = action.contact;
-                return {
-                    contactData: newItems,
-                    contacttypelist: state.contacttypelist,
-                };
-            }
-        }
-    }
-    else if (action.type === "delContactList") {
-        for (let i = 0; i < state.contactData.length; i++) {
-            if (action.contactId === state.contactData[i].ContactId) {
-                const newItems = [...state.contactData];
-                newItems.splice(i, 1);
-                return { contactData: newItems, contacttypelist: state.contacttypelist };
-            }
-        }
-    }
 
-    return state
 }
 
 const stateStore = createStore(stateStoreReducer);
