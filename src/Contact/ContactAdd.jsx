705a26ab2048ab83b8584656e32604cfac34e1ca
ericyu928 <84361126+ericyu928@users.noreply.github.com>
2022-03-24 09:56:13 +0800

    Form validator

--- src/Contact/ContactAdd.jsx
+++ src/Contact/ContactAdd.jsx
@@ -72,6 +72,32 @@ class ContactAdd extends React.Component {
         onEditContact(false)
 
     }
+    checkPhone = (rule, value) => {
+        console.log(value)
+        if (value === "") {
+            return Promise.reject("請輸入電話!");
+        }
+        else if (/^[09]{2}[0-9]{8,8}$/.test(value) || /^\++\d{6,15}$/.test(value)) {
+            return Promise.resolve();
+        }
+        else {
+            return Promise.reject("手機號碼格式錯誤!");
+        }
+
+    }
+    checkEmail = (rule, value) => {
+        console.log(value)
+        if (value === "") {
+            return Promise.reject("請輸入Email!");
+        }
+        else if (/^[\w.\-]+@(?:[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*\.)+[A-Za-z]{2,6}$/.test(value)) {
+            return Promise.resolve();
+        }
+        else {
+            return Promise.reject("Email格式錯誤!");
+        }
+
+    }
     render() {
         let { contactAddMode } = this.props;
         return (
@@ -82,22 +108,22 @@ class ContactAdd extends React.Component {
                 <Form.Item name="Classname" label="類別">
                     <Input className="readonly" readOnly="readonly" />
                 </Form.Item>
-                <Form.Item name="Name" label="姓名" rules={[{ required: true, message: '請輸入姓名'}]}>
-                    <Input type="text" placeholder="姓名"/>
+                <Form.Item name="Name" label="姓名" rules={[{ required: true, message: '請輸入姓名' }]}>
+                    <Input type="text" placeholder="姓名" />
                 </Form.Item>
-                <Form.Item name="Sex" label="性別" rules={[{ required: true, message: '請輸入性別'}]}>
+                <Form.Item name="Sex" label="性別" rules={[{ required: true, message: '請輸入性別' }]}>
                     <Select>
                         <Option value="男">男</Option>
                         <Option value="女">女</Option>
                     </Select>
                 </Form.Item>
-                <Form.Item name="Email" label="Email" rules={[{ required: true, message: '請輸入Email'}]}>
+                <Form.Item name="Email" label="Email" rules={[{ required: true, validator: this.checkEmail }]}>
                     <Input type="email" placeholder="Email" />
                 </Form.Item>
-                <Form.Item name="Phone" label="電話" rules={[{ required: true, message: '請輸入電話'}]}>
+                <Form.Item name="Phone" label="電話" rules={[{ required: true, validator: this.checkPhone }]}>
                     <Input type="text" placeholder="電話" />
                 </Form.Item>
-                <Form.Item name="Address" label="地址" rules={[{ required: true, message: '請輸入地址'}]}>
+                <Form.Item name="Address" label="地址" rules={[{ required: true, message: '請輸入地址' }]}>
                     <Input type="text" placeholder="地址" />
                 </Form.Item>
                 <Button type="primary" shape="round" htmlType="submit">儲存</Button>
